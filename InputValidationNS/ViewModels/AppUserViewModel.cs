12b50260fd412829a14e1f02c5bf5e3cfe84a047
Richard L. Taylor Jr <rtaylor@rightincode.com>
2017-08-17 18:00:34 -0400

    Implementing IValidatableObject

--- InputValidationNS/ViewModels/AppUserViewModel.cs
+++ InputValidationNS/ViewModels/AppUserViewModel.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using InputValidationNS.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace InputValidationNS.ViewModels
 {
-    public class AppUserViewModel
+    public class AppUserViewModel : IValidatableObject
     {
         private AppUser _AppUser;
 
@@ -14,10 +15,33 @@ namespace InputValidationNS.ViewModels
                 return _AppUser;
             }
         }
-                
+
+        public string FirstName
+        {
+            get { return _AppUser.firstName; }
+            set
+            {
+                _AppUser.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return _AppUser.lastName; }
+            set
+            {
+                _AppUser.lastName = value;
+            }
+        }
+
         public AppUserViewModel()
         {
             _AppUser = new AppUser();
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
         }
     }
 }
